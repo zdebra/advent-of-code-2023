0d52a3e8c64eb23d53daa9096a66219c7ad0b552
zdebra <zdenek@zdebra.cz>
2023-12-17 11:45:49 +0100

    fix

--- day02/target/debug/deps/day02-6a071c4ac48790c2.d
+++
@@ -1,5 +0,0 @@
-/Users/zdenek.brabecomio.com/dev/advent-of-code-2023/day02/target/debug/deps/day02-6a071c4ac48790c2.rmeta: src/main.rs
-
-/Users/zdenek.brabecomio.com/dev/advent-of-code-2023/day02/target/debug/deps/day02-6a071c4ac48790c2.d: src/main.rs
-
-src/main.rs:
